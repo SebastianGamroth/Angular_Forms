57a6ec76f0577c0cece42ca391ed01a1b76937e6
user7d11 <user7w11@gmail.com>
2022-09-18 13:39:58 +0200

    ControlValueAccessor

--- src/app/components/seat-place/seat-place.component.ts
+++ src/app/components/seat-place/seat-place.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-seat-place',
   templateUrl: './seat-place.component.html',
-  styleUrls: ['./seat-place.component.scss']
+  styleUrls: ['./seat-place.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SeatPlaceComponent),
+      multi: true
+    }
+  ]
 })
-export class SeatPlaceComponent {
+export class SeatPlaceComponent implements ControlValueAccessor {
+
+  public onTouched = () => { };
+  public onChange = (value: string) => { };
+  public touched: boolean = false;
+  public isDisabled: boolean = false;
+  public value: string = '';
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  markAsTouched(): void {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled
+  }
 
   image: any = [
     [1, 1, 1],
